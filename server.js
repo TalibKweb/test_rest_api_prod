00bd982780f090aab3ac3cf2b22a120e553213ce
TalibKweb <talib.kwebmaker@gmail.com>
2025-06-30 17:05:32 +0530

    added protected route functionality

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const dotenv = require('dotenv').config();
 const cors = require('cors');
 const userRoutes = require('./routes')
 const app = express();
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000;
 require('./db/db');
 
@@ -12,7 +13,55 @@ require('./db/db');
 // Middlewares
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser()); // ✅ Parses cookies
 app.use(express.urlencoded({ extended: true })); // To handle form submissions
+
+
+
+// >>>>>>>>>> Serve Login Page
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+
+// >>>>>>>>>> Validate Login Page for Rediretion
+app.post('/login', (req, res) => {
+  let { username, password } = req.body;
+
+  if (process.env.DASH_USERNAME === username &&
+    process.env.DASH_PASS === password) {
+
+    res.cookie('auth_token', process.env.SECRET_TOKEN, {
+      httpOnly: true, // JS can't access this
+      secure: false, // set to true in production with HTTPS
+    });
+
+    return res.redirect('/dashboard');
+  }
+  else {
+    res.status(401).send('Unauthorized: Invalid credentials');
+  }
+
+});
+
+
+// >>>>>>>>>> Serve Dashboard Page with Authentication
+
+app.get('/dashboard', (req, res) => {
+  if (req.cookies.auth_token !== process.env.SECRET_TOKEN) {
+    return res.redirect('/login');
+  }
+  else {
+    res.sendFile(path.join(__dirname, 'public', 'dashboard.html'));
+  }
+});
+
+// Logout Setup
+app.get('/logout', (req, res) => {
+  res.clearCookie('auth_token');
+  res.redirect('/login')
+
+});
 
 
 // Serve index.html at root
@@ -20,6 +69,7 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
+
 
 
 
